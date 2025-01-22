68b904df3eb1c4a309019c2a658d15cf2d4b3466
jayascodez <jayabrooks1@gmail.com>
2025-01-22 10:12:39 +0000

    adding journals

--- lib/models/journal_model.dart
+++ lib/models/journal_model.dart
@@ -2,12 +2,14 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class Journal {
+  final String id; // Add this field for the ID
   final String title;
   final String body;
   final String mood;
   final dynamic timestamp;
 
   Journal({
+    required this.id, // Pass id when creating a Journal object
     required this.title,
     required this.body,
     required this.mood,
@@ -16,6 +18,7 @@ class Journal {
 
   factory Journal.fromJson(Map<dynamic, dynamic> json) {
     return Journal(
+      id: json['id'] ?? '', // Assuming the API sends 'id'
       title: json['title'] ?? 'No Title',
       body: json['body'] ?? 'No Description',
       mood: json['mood'] ?? 'Unknown Mood',
@@ -25,10 +28,11 @@ class Journal {
 
   Map<String, dynamic> toJson() {
     return {
+      'id': id, // Include 'id' in the JSON payload if needed
       'title': title,
-      'body': title,
+      'body': body,
       'mood': mood,
-      'timestamp': title,
+      'timestamp': timestamp,
     };
   }
 }
@@ -50,10 +54,25 @@ class JournalRepository {
         // Map the entries list to Journal objects
         return entries.map((item) => Journal.fromJson(item)).toList();
       } else {
-        throw Exception('Failed to fetch journals. Status code: ${response.statusCode}');
+        throw Exception(
+            'Failed to fetch journals. Status code: ${response.statusCode}');
       }
     } catch (e) {
       throw Exception('Error fetching journals: $e');
+    }
+  }
+
+  Future<void> deleteJournal(String userId, String journalId) async {
+    try {
+      final String apiUrl = '$baseUrl/user/$userId/journal/$journalId';
+      final response = await http.delete(Uri.parse(apiUrl));
+
+      if (response.statusCode != 200 && response.statusCode != 204) {
+        throw Exception(
+            'Failed to delete journal entry: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Error deleting journal: $e');
     }
   }
 }
--- lib/views/journal_page.dart
+++ lib/views/journal_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import '../controllers/journal_controller.dart';
 
 class JournalScreen extends StatefulWidget {
@@ -16,14 +18,135 @@ class _JournalScreenState extends State<JournalScreen> {
 
   @override
   void initState() {
-    final user = FirebaseAuth.instance.currentUser;
-        if (user != null) {
-          final uid = user.uid;
     super.initState();
-    _journalController = Provider.of<JournalController>(context, listen: false);
-    _journalController.getJournals(uid); // Replace "0" with the actual user ID if needed
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      final uid = user.uid;
+      _journalController =
+          Provider.of<JournalController>(context, listen: false);
+      _journalController.getJournals(uid);
+    }
   }
+
+  Future<void> _refreshJournalList() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      final uid = user.uid;
+      await _journalController.getJournals(uid);
+    }
   }
+
+  Future<void> _deleteJournalEntry(String journalId) async {
+    try {
+      final user = FirebaseAuth.instance.currentUser;
+      if (user != null) {
+        final uid = user.uid;
+        final String baseUrl = 'https://api-rarz3eo25q-uc.a.run.app';
+        final String endpoint = '/user/$uid/journal/$journalId';
+        final Uri url = Uri.parse(baseUrl + endpoint);
+
+        final response = await http.delete(
+          url,
+          headers: {'Content-Type': 'application/json'},
+        );
+
+        if (response.statusCode == 200 || response.statusCode == 204) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+                content: Text("Journal entry deleted successfully!")),
+          );
+          await _refreshJournalList();
+        } else {
+          throw Exception(
+              'Failed to delete journal entry: ${response.statusCode}');
+        }
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("User not authenticated")),
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Failed to delete journal entry: $e")),
+      );
+    }
+  }
+
+// Show dialog to edit journal entry
+  Future<void> _showEditJournalDialog(
+      String journalId, String initialTitle, String initialBody) async {
+    final _titleController = TextEditingController(text: initialTitle);
+    final _descriptionController = TextEditingController(text: initialBody);
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Edit Journal Entry"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: _titleController,
+                decoration: const InputDecoration(labelText: "Title"),
+              ),
+              TextField(
+                controller: _descriptionController,
+                decoration: const InputDecoration(labelText: "Description"),
+                maxLines: 4,
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text("Cancel"),
+            ),
+            TextButton(
+              onPressed: () async {
+                try {
+                  final user = FirebaseAuth.instance.currentUser;
+                  if (user != null) {
+                    final uid = user.uid;
+                    final journalRef = FirebaseFirestore.instance
+                        .collection('users')
+                        .doc(uid)
+                        .collection('journal')
+                        .doc(journalId);
+
+                    await journalRef.update({
+                      'title': _titleController.text,
+                      'body': _descriptionController.text,
+                      'timestamp': FieldValue.serverTimestamp(),
+                    });
+
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text("Journal entry updated!")),
+                    );
+                    Navigator.pop(context); // Close the dialog
+                    await _refreshJournalList(); // Refresh the journal list
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text("User not authenticated")),
+                    );
+                  }
+                } catch (e) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                        content: Text("Failed to update journal entry: $e")),
+                  );
+                }
+              },
+              child: const Text("Save"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,14 +158,18 @@ class _JournalScreenState extends State<JournalScreen> {
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: ElevatedButton(
-              onPressed: () {
-                // Navigate to the form screen for creating a journal entry
-                Navigator.push(
+              onPressed: () async {
+                final isEntryAdded = await Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => const AddJournalEntryScreen(),
+                    builder: (context) => AddJournalEntryScreen(
+                        onEntryAdded: _refreshJournalList),
                   ),
                 );
+
+                if (isEntryAdded == true) {
+                  await _refreshJournalList();
+                }
               },
               child: const Text("Add New Entry"),
             ),
@@ -80,9 +207,51 @@ class _JournalScreenState extends State<JournalScreen> {
                           ),
                         ),
                         subtitle: Text(journal.mood),
-                        onTap: () {
-                          // Define the behavior when a journal is tapped
-                        },
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            IconButton(
+                              icon: const Icon(Icons.edit, color: Colors.blue),
+                              onPressed: () async {
+                                // Show the edit dialog
+                                await _showEditJournalDialog(
+                                  journal.id,
+                                  journal.title,
+                                  journal.body,
+                                );
+                              },
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.delete, color: Colors.red),
+                              onPressed: () async {
+                                final confirm = await showDialog<bool>(
+                                  context: context,
+                                  builder: (context) => AlertDialog(
+                                    title: const Text("Delete Journal Entry"),
+                                    content: const Text(
+                                        "Are you sure you want to delete this journal entry?"),
+                                    actions: [
+                                      TextButton(
+                                        onPressed: () =>
+                                            Navigator.of(context).pop(false),
+                                        child: const Text("Cancel"),
+                                      ),
+                                      TextButton(
+                                        onPressed: () =>
+                                            Navigator.of(context).pop(true),
+                                        child: const Text("Delete"),
+                                      ),
+                                    ],
+                                  ),
+                                );
+
+                                if (confirm == true) {
+                                  await _deleteJournalEntry(journal.id);
+                                }
+                              },
+                            ),
+                          ],
+                        ),
                       ),
                     );
                   },
@@ -97,7 +266,9 @@ class _JournalScreenState extends State<JournalScreen> {
 }
 
 class AddJournalEntryScreen extends StatefulWidget {
-  const AddJournalEntryScreen({super.key});
+  final VoidCallback onEntryAdded;
+
+  const AddJournalEntryScreen({super.key, required this.onEntryAdded});
 
   @override
   State<AddJournalEntryScreen> createState() => _AddJournalEntryScreenState();
@@ -117,7 +288,8 @@ class _AddJournalEntryScreenState extends State<AddJournalEntryScreen> {
           final uid = user.uid;
           print("The authenticated userid is: $uid");
 
-          await FirebaseFirestore.instance
+          // Add the journal entry and get the document reference
+          final docRef = await FirebaseFirestore.instance
               .collection('users')
               .doc(uid)
               .collection('journal')
@@ -127,11 +299,20 @@ class _AddJournalEntryScreenState extends State<AddJournalEntryScreen> {
             'mood': _selectedMood,
             'timestamp': FieldValue.serverTimestamp(),
           });
+          print(docRef.id);
+          // Update the document with its own ID
+          await docRef.set({
+            'id': docRef.id, // Set the 'id' field to the document ID
+            'title': _titleController.text,
+            'body': _descriptionController.text,
+            'mood': _selectedMood,
+            'timestamp': FieldValue.serverTimestamp(),
+          });
 
           ScaffoldMessenger.of(context).showSnackBar(
             const SnackBar(content: Text("Journal entry added successfully!")),
           );
-          Navigator.pop(context); // Go back to the journal list
+          Navigator.pop(context, true); // Notify the previous screen of success
         } else {
           ScaffoldMessenger.of(context).showSnackBar(
             const SnackBar(content: Text("User not authenticated")),
